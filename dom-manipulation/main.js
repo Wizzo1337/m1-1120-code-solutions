302e1619034f9845857155a6cbbbd14102ec376a
Thomas Willson <thomaswilllson@yahoo.com>
2021-01-09 12:43:01 -0800

    completed dom manipulation

---
+++ dom-manipulation/main.js
@@ -0,0 +1,35 @@
+var numberOfClicks = 0;
+
+var hotButton = document.querySelector('.hot-button');
+
+var clickCount = document.querySelector('.click-count');
+
+function handleClick(event) {
+  numberOfClicks++;
+}
+
+hotButton.addEventListener('click', handleClick);
+
+function handleUpdateText(event) {
+  clickCount.textContent = 'Clicks: ' + numberOfClicks;
+}
+
+hotButton.addEventListener('click', handleUpdateText);
+
+function handleUpdateButton(event) {
+  if (numberOfClicks < 4) {
+    hotButton.className = 'hot-button cold';
+  } else if (numberOfClicks < 7 && numberOfClicks >= 4) {
+    hotButton.className = 'hot-button cool';
+  } else if (numberOfClicks <= 10 && numberOfClicks >= 7) {
+    hotButton.className = 'hot-button tepid';
+  } else if (numberOfClicks <= 13 && numberOfClicks >= 10) {
+    hotButton.className = 'hot-button warm';
+  } else if (numberOfClicks <= 16) {
+    hotButton.className = 'hot-button hot';
+  } else {
+    hotButton.className = 'hot-button nuclear';
+  }
+}
+
+hotButton.addEventListener('click', handleUpdateButton);
