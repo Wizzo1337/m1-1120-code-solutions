a71908fef33c64ce3ab13ab5e7806611941f1ea1
Thomas Willson <thomaswilllson@yahoo.com>
2021-01-08 23:32:33 -0800

    completed dom events

---
+++ dom-events/buttons.js
@@ -0,0 +1,29 @@
+function handleClick(event) {
+  console.log('button clicked');
+  console.log(event);
+  console.log(event.target);
+}
+
+var button = document.querySelector('.click-button');
+
+button.addEventListener('click', handleClick, false);
+
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.log(event);
+  console.log(event.target);
+}
+
+var hover = document.querySelector('.hover-button');
+
+hover.addEventListener('mouseover', handleMouseover, false);
+
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log(event);
+  console.log(event.target);
+}
+
+var double = document.querySelector('.double-click-button');
+
+double.addEventListener('dblclick', handleDoubleClick, false);
